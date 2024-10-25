22672da92ee98c2a80ff2ddbfde7311b0ef1bcce
Yameen <yameendon@gmail.com>
2024-10-25 22:50:04 +0530

    commit with instance deployment and output update

--- otf-week-1/outputs.tf
+++ otf-week-1/outputs.tf
@@ -2,6 +2,6 @@ output "instance_arn" {
   value = aws_instance.ec2_instance.arn
   description = "ARN for deployed ec2 resource"
 }
-output "public ip" {
-  value = ""
+output "public-ip" {
+  value = "http://${aws_instance.ec2_instance.public_ip}"
 }
