12b4050626a84190d1a73083197ab5c7852020f8
Yameen <yameendon@gmail.com>
2024-11-10 16:05:36 +0530

    openvpn test working changed security group rules implementation. now utilizing the for_each and resource security_rules

--- open-vpn/outputs.tf
+++ open-vpn/outputs.tf
@@ -7,3 +7,11 @@ output "client_config_files" {
   description = "Path to client configuration files on the VPN server"
   value       = "/etc/openvpn/client-configs/"
 }
+output "scp_file_copy" {
+  description = "copy files from openvpn server to connect to vpn"
+  value       = "scp -i /c/Users/Yameen/Downloads/openvpn.pem ec2-user@${aws_instance.openvpn_server.public_ip}:/etc/openvpn/client-configs/* ./client-configs/"
+}
+output "ssh_login" {
+  description = "ssh and check if 5 files are available in client config"
+  value       = "ssh -i /c/Users/Yameen/Downloads/openvpn.pem ec2-user@${aws_instance.openvpn_server.public_ip}"
+}
